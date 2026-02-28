bd043fb5f364107c9afb598b1558f57922bd8a62
gon <vanthien7027@gmail.com>
2026-03-01 00:24:15 +0600

    feat/usecase cart belong domain layer

--- lib/features/cart/domain/usecases/get_cart_usecase.dart
+++ lib/features/cart/domain/usecases/get_cart_usecase.dart
@@ -1,5 +1,5 @@
 import 'package:app_fe_ecomerce/core/usecase/usecase.dart';
-import 'package:app_fe_ecomerce/features/cart/data/repositories/cart_repository.dart';
+import 'package:app_fe_ecomerce/features/cart/domain/repositories/cart_repository.dart';
 import 'package:fpdart/fpdart.dart';
 import '../../../../core/error/failures.dart';
 
---
+++ lib/features/cart/domain/usecases/update_cart_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:app_fe_ecomerce/core/usecase/usecase.dart';
+import 'package:app_fe_ecomerce/features/cart/domain/repositories/cart_repository.dart';
+import 'package:fpdart/fpdart.dart';
+import '../../../../core/error/failures.dart';
+
+class UpdateCartUseCase implements UseCase<void, UpdateCartParams> {
+  final CartRepository _repository;
+
+  UpdateCartUseCase(this._repository);
+
+  // Ví dụ hàm gọi Lấy danh sách Giỏ hàng
+  @override
+  Future<Either<Failure, void>> call(UpdateCartParams params) async {
+    return await _repository.updateCart(
+      id: params.id,
+      quantity: params.quantity,
+    );
+  }
+}
+
+class UpdateCartParams {
+  final int id;
+  final int quantity;
+
+  UpdateCartParams({required this.id, required this.quantity});
+}
